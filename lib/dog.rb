f5f180ebad3146143e2d88f9cd3dda6fe348f2a2
reflective-partial-8357 <scarter31191@gmail.com>
2020-07-02 02:02:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Dog
     def name= (dog_name)
         @name = dog_name
@@ -19,3 +20,20 @@ end
 
 
 
+=======
+class Dog 
+  def name= (dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+end 
+
+class Dog 
+  def bark
+    puts "woof!"
+  end
+end
+>>>>>>> b375a6f2a40067d4abca794da12c66ff3bc47959

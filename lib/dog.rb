bfbfafde2076a51e29e97f1854a328d2094b9b27
Steve Carter <Scarter31191@gmail.com>
2020-07-01 21:15:31 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Dog
+    def name= (dog_name)
+        @name = dog_name
+    end
+
+    def name
+        @name
+    end
+
+end
+    
+class Dog
+    def bark
+        puts "woof!"
+    end
+
+end
+
+
+
+
